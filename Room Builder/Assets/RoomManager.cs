56255480cc2998df4646a3f4329ff34d44f462ea
Anna Chen <iloveelmo1107@gmail.com>
2023-04-06 17:03:39 -0600

    others files

---
+++ Room Builder/Assets/RoomManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    public GameObject HeadwallSlideDoor;
+    public GameObject HeadwallSlideDoorPoints;
+    public GameObject HeadwallSwingDoor;
+    public GameObject HeadwallSwingDoorPoints;
+    public GameObject Headwall2SlideDoor;
+    public GameObject Headwall2SlideDoorPoints;
+    public GameObject Headwall2SwingDoor;
+    public GameObject Headwall2SwingDoorPoints;
+
+    public GameObject FootwallSlideDoor;
+    public GameObject FootwallSlideDoorPoints;
+    public GameObject FootwallSwingDoor;
+    public GameObject FootwallSwingDoorPoints;
+    public GameObject Footwall2SlideDoor;
+    public GameObject Footwall2SlideDoorPoints;
+    public GameObject Footwall2SwingDoor;
+    public GameObject Footwall2SwingDoorPoints;
+
+    public GameObject Player;
+    public GameObject IV_object;
+
+    public Vector3 PlayerLocalPositionFootwall = new Vector3(3.787f, 0.0f, 1.797f);
+    public Vector3 PlayerLocalPositionHeadwall = new Vector3(3.787f, 0.0f, 1.797f);
+    public string SubjectId;
+    public string SetNumber;
+    public enum IVenum { IV, NoIV };
+    public IVenum IV;
+    public enum SideEnum { Left, Right };
+    public SideEnum side;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (IV == IVenum.IV)
+        {
+            IV_object.SetActive(true);
+        }
+        else
+        {
+            IV_object.SetActive(false);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Tuple<string, string> GetPath()
+    {
+        ObjectPosition g = FindObjectOfType<ObjectPosition>();
+        string roomname = g.name;
+        string folder = Application.dataPath + "/CSV files/" + "Sub" + SubjectId.ToString() + "/Set" + SetNumber.ToString() + "/" + roomname.ToString() + "/" + IV.ToString() + "/" + side.ToString();
+        string hierarchyName = "Sub" + SubjectId.ToString() + "_Set" + SetNumber.ToString() + "_" + roomname.ToString() + "_" + IV.ToString() + "_" + side.ToString() + "_";
+
+        return new Tuple<string, string>(folder, hierarchyName);
+    }
+}
